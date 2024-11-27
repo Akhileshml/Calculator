077024f8a32a76b525111d365a4a2314423a61f5
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-11-27 17:17:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="Calculator",  # Replace with your package name
     version="0.1.0",  # Replace with your package version
     description="A simple calculator package",
-    author="Your Name",
-    author_email="your.email@example.com",
-    py_modules=["cal"],  # List your Python modules here
+    author="Akhi",
+    author_email="akhildevops15@gmail.com",
+    py_modules=["calku"],  # List your Python modules here
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
