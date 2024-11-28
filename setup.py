4a18c5364015c972c45fa8c9d5800df5dd053bcd
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-11-28 16:31:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='calculator',
-    version='1.0.0',
+    version='1.0.1',  # Incremented version
     packages=find_packages(),
     install_requires=[
         'pytest',
