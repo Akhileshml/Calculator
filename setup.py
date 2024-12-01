61898debc0b066b1e5618dfa20d76ab6568e2f37
Akhileshml <129209611+Akhileshml@users.noreply.github.com>
2024-12-01 20:57:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='calculator',
-    version='1.0.1',  # Incremented version
+    version='1.0.2',  # Incremented version
     packages=find_packages(),
     install_requires=[
         'pytest',
     ],
-    description='A calculator package',
+    description='Simple cal package',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
